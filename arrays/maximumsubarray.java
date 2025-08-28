76a23ed0de3b97a3ae27fd6bce9a76c086105295
vaish7github <vaishnavitiwari968@gmail.com>
2025-08-28 21:28:23 +0530

    upd

--- arrays/maximumsubarray.java
+++ arrays/maximumsubarray.java
@@ -3,13 +3,16 @@ class Main{
     public static void main(String [] args){
         Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
-        int [] nums= new int[n];
+        int [] nums= new int[n];   
+        
         for (int i =0;i< n;i++){
             nums[i]= sc.nextInt();
         }
         sc.close();
         int result = maxsubarray(nums);
         System.out.println(result);
+
+        // this is an edit 
     }
     public static int maxsubarray(int [] nums){
         int curr = nums[0];
